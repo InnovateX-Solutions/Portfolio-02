1e785a9c68d570c1f85ca55a3b13dc63d95f40cb
InnovateX-Developer <Info.fxdeveloper2025@gmail.com>
2025-05-05 17:03:12 +0500

    Portfolio Updated

--- Assets/Js/script.js
+++ Assets/Js/script.js
@@ -1,12 +1,3 @@
-function showsidebar(){
-    const side = document.getElementById("sidebar");
-    side.style.display = "flex"
-}
-function hidesidebar(){
-    const side = document.getElementById("sidebar");
-    side.style.display = "none"
-};
-
 
 const cursorDot = document.querySelector("[data-cursor-dot]");
 const cursorOutline = document.querySelector("[data-cursor-outline]");
@@ -18,8 +9,8 @@ window.addEventListener("mousemove",function(e){
  cursorDot.style.left = `${posX}px`;
  cursorDot.style.top = `${posY}px`;
 
-//  cursorOutline.style.left = `${posX}px`;
-//  cursorOutline.style.top = `${posY}px`;
+ cursorOutline.style.left = `${posX}px`;
+ cursorOutline.style.top = `${posY}px`;
 
 cursorOutline.animate({
     left:`${posX}px`,
@@ -29,59 +20,11 @@ cursorOutline.animate({
 });
 
 
-const top = document.getElementById("to-top")
-window.addEventListener("scroll", () =>{
-if(window.pageYOffset > 80){
-    top.classList.add("active")
-}else{
-    top.classList.remove("active")
+function showsidebar(){
+    const side = document.getElementById("sidebar");
+    side.style.display = "flex"
 }
-});
-
-function project(){
-    const btnProject = document.getElementById("intro-book-btn0");
-    const btnExp = documen.getElementById("intro-book-btn01");
-    const btnskill = documen.getElementById("intro-book-btn02");
-    const projectRow = document.getElementById("project-row");
-    const expRow = document.getElementById("exp-row");
-    const mySkillRow = document.getElementById("skills-row");
-
-
-    expRow.style.display = "none"
-    mySkillRow.style.display = "none"
-    projectRow.style.display = "flex"
-
-
-};
-function Exp(){
-    const btnProject = document.getElementById("intro-book-btn0");
-    const btnExp = documen.getElementById("intro-book-btn01");
-    const btnskill = documen.getElementById("intro-book-btn02");
-    const projectcard1 = document.getElementById("card-project02");
-    const projectcard = document.getElementById("card-project01");
-    const expRow = document.getElementById("exp-row");
-    const mySkillRow = document.getElementById("skills-row");
-
-
-    expRow.style.display = "flex"
-    mySkillRow.style.display = "none"
-    projectcard.style.display = "none"
-    projectcard1.style.display = "none"
-
-
-};
-function mySkils(){
-    const btnProject = document.getElementById("intro-book-btn0");
-    const btnExp = documen.getElementById("intro-book-btn01");
-    const btnskill = documen.getElementById("intro-book-btn02");
-    const projectRow = document.getElementById("project-row");
-    const expRow = document.getElementById("exp-row");
-    const mySkillRow = document.getElementById("skills-row");
-
-
-    expRow.style.display = "none"
-    mySkillRow.style.display = "flex"
-    projectRow.style.display = "none"
-
-
+function hidesidebar(){
+    const side = document.getElementById("sidebar");
+    side.style.display = "none"
 };
